32beb5c9eb2fbd71491ab8f37e90757f3ba419d1
QWERT12435 <117696181+QWERT12435@users.noreply.github.com>
2022-12-13 19:34:19 -0500

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyC9ibOcwNWvKsGO1AZZqBSGV62Qf5Lyh48",
+    authDomain: "kwitter-34c83.firebaseapp.com",
+    databaseURL: "https://kwitter-34c83-default-rtdb.firebaseio.com",
+    projectId: "kwitter-34c83",
+    storageBucket: "kwitter-34c83.appspot.com",
+    messagingSenderId: "518103856539",
+    appId: "1:518103856539:web:f6dca217711a2229c2dd3e"
+  };
+  
+  // Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+  
